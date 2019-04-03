1e5afb06e44ca5a4f9114789d1103bf28b024ee7
Ben Staniforth <benstaniforth@Bens-MacBook-Pro.local>
2019-04-03 15:21:39 +0100

    Stuff

--- src/main/java/softwire/training/myface/models/viewmodels/WallViewModel.java
+++ src/main/java/softwire/training/myface/models/viewmodels/WallViewModel.java
@@ -1,6 +1,7 @@
 package softwire.training.myface.models.viewmodels;
 
 import softwire.training.myface.models.dbmodels.Post;
+import softwire.training.myface.models.dbmodels.Users;
 
 import java.util.List;
 
@@ -10,4 +11,13 @@ public class WallViewModel {
     public String wallOwnerUsername;
     public List<Post> posts;
 
+    private List<Users> allUsers;
+
+    public List<Users> getAllUsers() {
+        return allUsers;
+    }
+
+    public void setAllUsers(List<Users> allUsers) {
+        this.allUsers = allUsers;
+    }
 }
