afa07d370340bc0fe488545a528e0468d6b96210
Ben Staniforth <benstaniforth@Bens-MacBook-Pro.local>
2019-04-03 11:49:29 +0100

    Some stuff

--- src/main/java/softwire/training/myface/controllers/NewUserController.java
+++ src/main/java/softwire/training/myface/controllers/NewUserController.java
@@ -1,15 +1,21 @@
 package softwire.training.myface.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
+import softwire.training.myface.services.UsersService;
 
 @Controller
 @RequestMapping(value = "/signup")
 public class NewUserController {
+    private final UsersService usersService;
+
+    @Autowired
+    public NewUserController (UsersService usersService) {this.usersService = usersService;}
 
     @RequestMapping(value = "", method = RequestMethod.GET)
     public ModelAndView showSignUpPage() {
@@ -25,6 +31,11 @@ public class NewUserController {
     ) {
 
         // TODO: Implement sign-up!
+
+
+        
+
+        usersService.addNewUser(user);
 
         return new RedirectView("/users");
     }
